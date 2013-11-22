18dbb459a29d295db423d050e2708edfbd63dad2
sunk <sunk@debian.phub.net.cable.rogers.com>
2013-11-21 21:43:53 -0500

    updated ignore file

---
+++ testbin/vectormult/depend.mk
@@ -0,0 +1,13 @@
+
+vectormult.o: \
+ vectormult.c \
+ $(OSTREE)/include/unistd.h \
+ $(OSTREE)/include/sys/types.h \
+ $(OSTREE)/include/machine/types.h \
+ $(OSTREE)/include/kern/types.h \
+ $(OSTREE)/include/kern/unistd.h \
+ $(OSTREE)/include/kern/ioctl.h \
+ $(OSTREE)/include/stdio.h \
+ $(OSTREE)/include/stdarg.h \
+ $(OSTREE)/include/err.h
+
